7da0f71879422be98bbd3fd45f8be14adef799f4
Neil Sant Gat <negat@microsoft.com>
2016-02-24 19:49:34 +0000

    added retry logic to script

--- CentOS-2nics-lb-cluster/deploy.sh
+++ CentOS-2nics-lb-cluster/deploy.sh
@@ -3,4 +3,9 @@ sed -i "s/^DEVICE=.*/DEVICE=eth1/I" "/etc/sysconfig/network-scripts/ifcfg-eth1"
 defaultgw=$(ip route show |sed -n "s/^default via //p")
 declare -a gateway=(${defaultgw// / })
 sed -i "\$aGATEWAY=${gateway[0]}" /etc/sysconfig/network
-service network restart
+until service network restart
+do
+  echo "Try again"
+  sleep 2
+done
+
