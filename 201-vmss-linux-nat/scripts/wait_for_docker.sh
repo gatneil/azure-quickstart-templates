a03fa21a873561156c603b4cf7b1900ab271d967
negat <negat@microsoft.com>
2017-03-28 22:04:22 +0000

    added comments

--- 201-vmss-linux-nat/scripts/wait_for_docker.sh
+++ 201-vmss-linux-nat/scripts/wait_for_docker.sh
@@ -1,5 +1,10 @@
+# wait a bit to make sure the Docker extension folder exists
+sleep 10
+
+# find the Docker folder (if you have auto-upgrade-minor-version set to false, this can be hard-coded)
 status_file_path=/var/lib/waagent/$(ls /var/lib/waagent | grep Microsoft.Azure.Extensions.Docker | grep -v xml)/status/0.status
 
+# wait until docker extension is finished
 while [ 1 == 1 ]
 do
 	if grep -q "success" $status_file_path; then
@@ -10,4 +15,5 @@ do
 	fi
 done
 
+# be awesome
 echo 'docker finished; do cool stuff here'
